26263cc07de75e8efa7b8c05851f179186650e06
rishabh0009-7 <rizzlifts2024@gmail.com>
2025-06-24 18:57:29 +0530

    struct

--- 1-Basics/contracts/6-struct.sol
+++ 1-Basics/contracts/6-struct.sol
@@ -1,1 +1,28 @@
 pragma solidity ^0.8.0;
+
+
+
+contract struct{
+    struct Person {
+        string name
+        uint age;
+        address addr;
+    }
+
+    Person  public person 
+
+    function setPerson(string memory _name, uint _age, address _addr ) public {
+        Person.name = _name ;
+        Person.age = _age ;
+        Person.addr = _addr
+
+    }
+
+    function getPerson() public view returns(string memory , uint , address{
+        return (Person.name , Person.age , Person.addr)
+
+    }
+
+
+
+}
